b2c5832e7b892c796b4dd1faeedc310b1510d416
Han <114632012+hann-le@users.noreply.github.com>
2025-07-05 03:33:35 -0400

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, render_template, send_from_directory
 import os
 
@@ -10,15 +9,14 @@ def index():
 
 @app.route("/download")
 def download():
-    # Serve the securiscan.py script for download
     return send_from_directory(directory=os.path.join(app.root_path, 'static'),
                                path='SafePort.py', as_attachment=True)
 
 @app.route("/downloadexe")
 def downloadexe():
-    # Placeholder: serve securiscan.exe if you build it and put it in static folder
     return send_from_directory(directory=os.path.join(app.root_path, 'static'),
                                path='SafePort.exe', as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))  # Use Render's dynamic port
+    app.run(host="0.0.0.0", port=port)
